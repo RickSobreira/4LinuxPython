b8170d639dc5ca6f9f39c90107c936f17d611390
RickSobreira <ricasobreira@gmail.com>
2019-09-16 22:19:23 -0300

    Primeiro commit

---
+++ ex01.py
@@ -0,0 +1,79 @@
+# x= int(input("Digite o primeiro valor da soma: "))
+# y= int(input("Digite o segundo valor da soma: "))
+# print( x + y)
+
+
+# num1 = 10
+# num2 = 30
+# print (num1 < num2)
+
+
+# num1 = 10
+# num2 = 1
+# if num1 > num2:
+#    print("O primeiro valor é maior!")
+# else:
+#    print("O primeiro valor na verdade é menor!")
+ 
+#x = int(input("Digite sua idade: "))
+#if x >= 18:
+#    y = input("Responda com Sim ou Não. Possui habilitação? ")
+#    if y == "Sim":
+#        print("Você pode dirigir!")
+#    else:
+#        print("Você precisa tirar a CNH!")
+#else:
+#   print("Você ainda não tem idade para dirigir!")
+
+#x = int(input("Digite sua idade: "))
+#y = input("Responda com Sim ou Não. Possui habilitação: ")
+#if x >=18 and y == "Sim":
+#    print("Você está apto a dirigir!")
+#else:
+#    print("Você precisa ser maior de idade ou tirar sua CNH!")
+
+
+#x = int(input("Digite sua idade: "))
+#y = input("Responda com Sim ou Não. Possui habilitação: ")
+#if x >=18 or y == "Sim":
+#    print("Você está apto a dirigir!")
+#else:
+#    print("Você precisa ser maior de idade ou tirar sua CNH!")
+ 
+nota1 = 5
+nota2 = 5.5
+nota3 = 4
+
+media = float(nota1 + nota2 + nota3)/3
+if media > 6:
+    print("Aprovado com: ",media)
+    if media < 4:
+        print("Reprovado com: ",media)
+else:
+    print("Recuperação com: ",media)
+    
+    
+ 
+ 
+ 
+ 
+ 
+ 
+    
+        
+
+
+
+
+
+
+
+
+
+   
+    
+    
+    
+
+
+
