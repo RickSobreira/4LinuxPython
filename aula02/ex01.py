c820887d3e99d0d559022093cc7d46d5899ae421
RickSobreira <ricasobreira@gmail.com>
2019-09-17 21:58:46 -0300

    aula de loop

---
+++ aula02/ex01.py
@@ -0,0 +1,69 @@
+#ano = int(input("Qual o seu ano de nascimento? (informar no padrão xxxx) "))
+#if ano <= 1966:
+#    print("Vocẽ é da geração BB!")
+#elif ano > 1966 and ano <= 1980:
+#    print("Você é da geração X!")
+#elif ano > 1980 and ano <= 1999:
+#    print("Você é da geração Y!")
+#else:
+#    print("Você é da geração Z!")
+
+# CALCULADORA
+#x = int(input("Digite o primeiro número: "))
+#y = int(input("Digite o segundo número: "))
+#z = int(input("Digite 1 para soma, 2 para subtracao, 3 para multiplicacao, 4 para divisão, 5 para sair: "))
+#if z == 1:
+#    print("O valor da soma é {}".format(x + y))
+#elif z == 2:
+#    print("O valor da subtracao é {}".format(x-y))
+#elif z == 3:
+#    print("O valor da multiplicação é {}".format(x*y))
+#elif z == 4:
+#    print("O valor da divisão é {}" .format(x/y))
+#else:
+#    print("Sair do programa")
+
+# CALCULADORA COM WHILE
+#while True:
+#    x = int(input("Digite o primeiro número: "))
+#    y = int(input("Digite o segundo número: "))
+#    z = int(input("Digite 1 para soma, 2 para subtracao, 3 para multiplicacao, 4 para divisão, 5 para sair: "))
+#    if z == 1:
+#        print("O valor da soma é {}".format(x + y))
+#    elif z == 2:
+#        print("O valor da subtracao é {}".format(x-y))
+#    elif z == 3:
+#        print("O valor da multiplicação é {}".format(x*y))
+#    elif z == 4:
+#        print("O valor da divisão é {}" .format(x/y))
+#    elif z == 5:
+#        print("Sair do programa!")
+#        break
+#    else:
+#        print("Digite uma operação válida (1,2,3,4,5)!")
+ 
+
+ 
+        
+        
+            
+            
+        
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
